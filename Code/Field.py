339e24f41875a241f246b49027578488c2ce32e0
Fuyutaru <zijian1105@gmail.com>
2024-04-30 11:32:14 +0200

    changement de field

--- Code/Field.py
+++ Code/Field.py
@@ -1,19 +1,11 @@
-<<<<<<<< HEAD:Code/Field.py
+
 class Field():
     def __init__(self, name, type, path, value):
         self.name = name
         self.type = type
         self.path = path
         self._value = value
-========
-class Champ():
-    def __init__(self, nom, type, path):
-        self.nom = nom
-        self.type = type
-        self.path = path
-        self._valeur = None
->>>>>>>> Tableau:Code/miseEnCommunGUI/Champ.py
-    
+
     @property
     def valeur(self):
         return self._valeur
