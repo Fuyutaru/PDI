31b249a085cb3c6009189f40c46994040f67034b
MaelleKlein <kleinmaelle25@gmail.com>
2024-05-13 12:17:44 +0200

    Suppression fichiers de tests de mise en commun

--- Code/miseEnCommunGUI/Enumeration.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 16:31:33 2024
-
-@author: Svetie
-"""
-
-enumDict = {'enum_TypeCodedTarget': ["IGNIndoor","IGNDroneSym","IGNDroneTop","CERN"],
-                         'enum_SysCoGeo':["Lamber93", "RTL", "GeoC", "Local"]}
