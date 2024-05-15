48a6327a12010ba43f9cb4f437bfbb2ce5bb42c7
Fuyutaru <zijian1105@gmail.com>
2024-05-15 10:38:17 +0200

    bool

--- Code/Enumeration.py
+++ Code/Enumeration.py
@@ -3,6 +3,7 @@
 
 class enum:
     enumDict = {'enum_TypeCodedTarget': ['"IGNIndoor"','"IGNDroneSym"','"IGNDroneTop"','"CERN"'],
-                         'enum_SysCoGeo':['"Lamber93"', '"RTL"', '"GeoC"', '"Local"']}
-    enumName = ['enum_TypeCodedTarget', 'enum_SysCoGeo']
+                         'enum_SysCoGeo':['"Lamber93"', '"RTL"', '"GeoC"', '"Local"'],
+                         'bool': ["True", "False", "1", "0"]}
+    enumName = ['enum_TypeCodedTarget', 'enum_SysCoGeo', 'bool']
 
